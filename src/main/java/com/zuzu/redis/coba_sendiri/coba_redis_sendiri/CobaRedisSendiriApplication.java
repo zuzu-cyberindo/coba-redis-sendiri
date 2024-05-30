59c980852ed2c257943bedf63fda773b15cd0616
zuzu <zuriel@cyberindo-sinergi.com>
2024-05-30 15:34:56 +0700

    update :

--- src/main/java/com/zuzu/redis/coba_sendiri/coba_redis_sendiri/CobaRedisSendiriApplication.java
+++ src/main/java/com/zuzu/redis/coba_sendiri/coba_redis_sendiri/CobaRedisSendiriApplication.java
@@ -2,7 +2,6 @@ package com.zuzu.redis.coba_sendiri.coba_redis_sendiri;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 public class CobaRedisSendiriApplication {
